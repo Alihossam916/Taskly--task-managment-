24e76c864c537be98ba33f9fbffdab2d01061083
Alihossam916 <alihossam916@gmail.com>
2026-06-02 01:46:24 +0300

    style: change input and button padding and border radius for mobile devices

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -12,7 +12,7 @@ export default function Button({
   ...props
 }: ButtonProp) {
   const styles =
-    "w-32 px-4 py-2 font-semibold rounded-sm transition-all duration-200 cursor-pointer";
+    "w-32 px-4 py-4 sm:py-2 font-semibold rounded-xl sm:rounded-sm transition-all duration-200 cursor-pointer";
   const variants = {
     primary: "bg-primary text-white hover:bg-primary-container",
     secondary: "bg-background text-primary hover:bg-surface-highest",
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -7,7 +7,7 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 const Input = forwardRef<HTMLInputElement, InputProps>(
   ({ variant = "primary", className, ...props }, ref) => {
     const styles =
-      "w-full px-4 py-2 font-medium rounded-sm transition-all outline-none";
+      "w-full px-4 py-4 sm:py-2 font-medium rounded-xl sm:rounded-sm transition-all outline-none";
     const variants = {
       primary:
         "bg-[#D7E2FF] text-[#6B7280] focus:ring-2 focus:ring-primary-container",
