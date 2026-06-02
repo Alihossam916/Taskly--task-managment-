2fb7226882d19bc54453bec909b5c10db82c21ed
Alihossam916 <alihossam916@gmail.com>
2026-06-02 17:09:46 +0300

    style: change padding and text for mobile view

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -3,15 +3,24 @@ import Link from "next/link";
 
 const SignUp = () => {
   return (
-    <div className="flex flex-col items-center gap-4 py-12 rounded-sm sm:bg-white max-w-xl mx-auto mb-12 sm:shadow-lg">
-      <h1 className="headline-lg">Create your workspace</h1>
-      <p className="body-md text-slate-2">
-        Join the editorial approach to task management.
-      </p>
+    <div className="flex flex-col items-center gap-4 py-12 px-4 sm:px-12 rounded-sm sm:bg-white max-w-xl mx-auto mb-12 sm:shadow-lg">
+      <div className="flex flex-col gap-4">
+        <h1 className="headline-lg">Create your workspace</h1>
+        <p className="body-md text-slate-2 text-center hidden sm:block">
+          Join the editorial approach to task management.
+        </p>
+        <p className="body-md text-slate-2 sm:hidden">
+          Join the curated environment for institutional trust and task
+          precision.
+        </p>
+      </div>
       <SignUpForm />
       <div className="flex items-center gap-1">
         <p className="body-md text-slate-2">Already have an account?</p>
-        <Link href={"/login"} className="text-primary font-bold hover:underline">
+        <Link
+          href={"/login"}
+          className="text-primary font-bold hover:underline"
+        >
           Log in
         </Link>
       </div>
