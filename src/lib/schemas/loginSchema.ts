2457babfcc5f48a047153ffe37a6781d49d322d7
Alihossam916 <alihossam916@gmail.com>
2026-06-02 21:43:59 +0300

    refactor: add rememberMe field to login schema

--- src/lib/schemas/loginSchema.ts
+++ src/lib/schemas/loginSchema.ts
@@ -3,6 +3,7 @@ import * as z from "zod";
 export const loginSchema = z.object({
   email: z.string().email({ message: "invalid email address" }),
   password: z.string().min(1, "Password cannot be empty"),
+  rememberMe: z.boolean(),
 });
 
 export type LoginFormData = z.infer<typeof loginSchema>;
