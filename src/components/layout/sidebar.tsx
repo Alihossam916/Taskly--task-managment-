a0dfb92e218fd35bf61766527498d36125459e5f
Alihossam916 <alihossam916@gmail.com>
2026-06-05 17:35:24 +0300

    refactor: update sidebar component to use local state and handle logout logic

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,67 +1,101 @@
 "use client";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+import { toast } from "react-toastify";
+
+// libs
+import { navLinks } from "@/src/lib/constants/navLinks";
+import { getAccessToken } from "@/src/lib/utils/cookies";
+import { logoutApi } from "@/src/lib/api/auth";
 
 // icons
+import Logo from "../icons/logo";
+import LogoutIcon from "../icons/logoutIcon";
+import CollapseIcon from "../icons/collapseIcon";
+import ExpandIcon from "../icons/expandIcon";
 
 const Sidebar = () => {
-  const { extended, toggleSidebar } = useSidebarStore(); // Access the sidebar state and toggle function from sidebarStore
-  const { user } = useAuthStore(); // Access the user state from authStore
+  const router = useRouter();
+  const [extended, setExtended] = useState(true);
+
+  async function handleLogout() {
+    const token = await getAccessToken();
+    if (!token) {
+      toast.error("No session found. Please log in again.");
+      router.push("/login");
+      return;
+    }
+    const response = await logoutApi(token);
+    if (response.error) {
+      toast.error("Logout failed, please try again.");
+      return;
+    }
+    toast.success("Logout successfully");
+    router.push("/login");
+  }
 
   return (
     <aside className="hidden sm:block fixed top-0 z-50">
       <nav
-        className={`${extended ? "w-52" : "w-16"} h-screen bg-background border-r-2 border-border p-4 transition-all duration-300`}
+        className={`${extended ? "w-64" : "w-20"} flex flex-col justify-between h-screen bg-surface-low p-4 transition-all duration-300`}
       >
-        {/* Sidebar toggle button */}
-        <button
-          onClick={toggleSidebar}
-          className={`${extended ? "ml-36" : "-ml-2 "} mb-4 hover:bg-primary hover:text-primary-foreground transition-all duration-300 p-2 rounded-sm cursor-pointer`}
-        >
-          {extended ? <PanelRightOpen /> : <BookOpen />}
-        </button>
-        <hr className="-ml-0.5 mb-2 border-2 w-full" /> {/* Navigation links */}
-        <ul className="space-y-4 overflow-x-hidden">
-          <li>
-            <Link
-              href="/"
-              className="flex items-center gap-4 w-full text-lg font-semibold text-foreground hover:text-primary hover:bg-primary/ transition-colors duration-200"
-            >
-              <House />
-              {extended && "Home"}
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/courses"
-              className="flex items-center gap-4 w-full text-lg font-semibold text-foreground hover:text-primary hover:bg-primary/ transition-colors duration-200"
-            >
-              <School />
-              {extended && "Courses"}
-            </Link>
-          </li>
-          {user && (
-            <li>
-              <Link
-                href="/dashboard"
-                className="flex items-center gap-4 w-full text-lg font-semibold text-foreground hover:text-primary hover:bg-primary/ transition-colors duration-200"
-              >
-                <LayoutDashboard />
-                {extended && "Dashboard"}
-              </Link>
-            </li>
-          )}
-        </ul>
-        <hr className="-ml-0.5 my-2 border-2 w-full" />{" "}
-        {/* Additional links or content can be added here */}
-        {!user && (
-          <Link
-            href="/auth/login"
-            className="flex items-center gap-4 w-full text-lg font-semibold text-foreground hover:text-primary hover:bg-primary/ transition-colors duration-200"
-          >
-            <LogIn />
-            {extended && "Sign In"}
+        <div>
+          <Link href={"/"} className="flex gap-2 items-center w-fit ml-2">
+            <Logo className="size-7" />
+            {extended && (
+              <h2 className="headline-lg font-bold uppercase">Taskly</h2>
+            )}
           </Link>
-        )}
+
+          <ul className="space-y-2 mt-5">
+            {navLinks.map((link) => {
+              return (
+                <Link
+                  key={link.name}
+                  href={`${link.href}`}
+                  className="group block w-full text-lg font-semibold text-foreground rounded-sm p-3 focus:bg-white hover:bg-white transition-colors duration-200 cursor-pointer"
+                >
+                  <li className="flex items-center gap-2">
+                    <link.icon className="size-6 group-hover:text-primary-container group-focus:text-primary-container" />
+                    {extended && (
+                      <span className="group-hover:text-primary">
+                        {link.desktopName}
+                      </span>
+                    )}
+                  </li>
+                </Link>
+              );
+            })}
+          </ul>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              setExtended(!extended);
+            }}
+            className="group flex items-center gap-2 cursor-pointer hover:bg-white transition-colors duration-200 p-3 w-full rounded-sm"
+          >
+            {extended ? (
+              <CollapseIcon className="group-hover:text-primary-container" />
+            ) : (
+              <ExpandIcon className="group-hover:text-primary-container" />
+            )}
+            {extended && (
+              <span className="title-md text-slate-3 group-hover:text-primary-container">
+                Collapse
+              </span>
+            )}
+          </button>
+          <button
+            onClick={handleLogout}
+            className="flex items-center gap-2 cursor-pointer focus:bg-white hover:bg-white transition-colors duration-200 p-3 w-full rounded-sm"
+          >
+            <LogoutIcon />
+            {extended && <span className="title-md text-error">Logout</span>}
+          </button>
+        </div>
       </nav>
     </aside>
   );
