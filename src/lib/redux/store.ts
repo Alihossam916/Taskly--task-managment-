34030ffcb6d3db7bd230be8e68c8ee4e59e91b1d
Alihossam916 <alihossam916@gmail.com>
2026-06-05 18:47:57 +0300

    refactor: add sidebar state management slice in redux store

--- src/lib/redux/store.ts
+++ src/lib/redux/store.ts
@@ -1,14 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from "@reduxjs/toolkit";
+import sidebarReducer from "./feature/sidebarSlice";
 
 export const makeStore = () => {
-  const dummyReducer = (state = {}) => state;
   return configureStore({
-    reducer: {dummyReducer},
-  })
-}
+    reducer: { sidebar: sidebarReducer },
+  });
+};
 
 // Infer the type of makeStore
-export type AppStore = ReturnType<typeof makeStore>
+export type AppStore = ReturnType<typeof makeStore>;
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<AppStore['getState']>
-export type AppDispatch = AppStore['dispatch']
+export type RootState = ReturnType<AppStore["getState"]>;
+export type AppDispatch = AppStore["dispatch"];
