24dd0791ba56f37251b3acfbac87392fe525d4cd
Alihossam916 <alihossam916@gmail.com>
2026-06-09 02:17:04 +0300

    feat:  create addProject API function to handle project creation requests

---
+++ src/lib/api/projects/addProject.ts
@@ -0,0 +1,37 @@
+"use server";
+// utils
+import { getAccessToken } from "../../utils/cookies";
+// types
+import { AddProjectFormData } from "../../schemas/addProjectSchema";
+
+const baseUrl = process.env.SUPABASE_URL!;
+const anonKey = process.env.SUPABASE_ANON_KEY!;
+
+export async function addProjectApi(formData: AddProjectFormData) {
+  const { name, description } = formData;
+  const token: string | null = await getAccessToken();
+  try {
+    const response = await fetch(`${baseUrl}/rest/v1/projects`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        apikey: anonKey,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        description,
+      }),
+    });
+    if (response.ok) {
+      const user = await response.json();
+      return user;
+    }
+  } catch (error) {
+    return {
+      error: {
+        message: error instanceof Error && error.message,
+      },
+    };
+  }
+}
