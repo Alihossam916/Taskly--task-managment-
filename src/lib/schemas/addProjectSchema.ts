8da118289b974ed500c20f0f323220b00612c4ab
Alihossam916 <alihossam916@gmail.com>
2026-06-09 02:17:24 +0300

    refactor: Update validation messages in addProjectSchema

--- src/lib/schemas/addProjectSchema.ts
+++ src/lib/schemas/addProjectSchema.ts
@@ -3,11 +3,11 @@ import * as z from "zod";
 export const addProjectSchema = z.object({
   name: z
     .string()
-    .min(3, "Name must be at least 3 characters ")
-    .max(100, "Name must be at most 100 characters"),
+    .min(3, "Project name must be at least 3 characters.")
+    .max(100, "Project name must be at most 100 characters"),
   description: z
     .string()
-    .max(500, "Description must be at most 500 characters"),
+    .max(500, "Description must be at most 500 characters").optional(),
 });
 
 export type AddProjectFormData = z.infer<typeof addProjectSchema>;
