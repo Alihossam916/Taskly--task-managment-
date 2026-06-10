eae5cd18ae6c9a139c2ef6dcc30f39d67f87a27d
Alihossam916 <alihossam916@gmail.com>
2026-06-10 15:40:33 +0300

    feat: add getProjects API function to fetch projects with pagination support

---
+++ src/lib/api/projects/getProjects.ts
@@ -0,0 +1,35 @@
+"use server";
+// utils
+import { getAccessToken } from "../../utils/cookies";
+
+const baseUrl = process.env.SUPABASE_URL!;
+const anonKey = process.env.SUPABASE_ANON_KEY!;
+
+export async function getProjects(limit: number, offset: number) {
+  const token: string | null = await getAccessToken();
+
+  try {
+    const response = await fetch(
+      `${baseUrl}/rest/v1/rpc/get_projects?limit=${limit}&offset=${offset}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          apikey: anonKey,
+          "Content-Type": "application/json",
+          Prefer: "count=exact",
+        },
+      },
+    );
+    if (response.ok) {
+      const projects = await response.json();
+      const total =
+        Number(response.headers.get("content-range")?.split("/")[1]) || 0;
+      return { projects, total };
+    }
+  } catch (error) {
+    throw new Error(
+      error instanceof Error ? error.message : "Failed to fetch projects",
+    );
+  }
+}
