1fc408af269e07137cbb822338f12918f3d0787a
Alihossam916 <alihossam916@gmail.com>
2026-06-11 01:20:37 +0300

    feat: ceate project edit page and form

---
+++ src/app/(main)/project/[projectId]/edit/page.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+
+// icons
+import AddMemberIcon from "@/src/components/icons/addMemberIcon";
+
+// components
+import Button from "@/src/components/ui/button";
+import EditProjectForm from "@/src/components/common/projects/editProjectForm";
+
+// lib
+import { getProjectById } from "@/src/lib/api/projects/getProjectById";
+
+interface Props {
+  params: Promise<{ projectId: string }>;
+}
+
+export async function ProjectEditPage({ params }: Props) {
+  const { projectId } = await params;
+  const project = await getProjectById(projectId);
+
+  return (
+    <div>
+      <div className="flex items-center gap-2">
+        <Link href={"/project"} className="uppercase label-sm text-slate-2">
+          Projects
+        </Link>
+        <span className="text-slate-1">{">"}</span>
+        <p className="uppercase label-sm text-primary">edit project</p>
+      </div>
+      <div className="hidden sm:flex items-center justify-between">
+        <h1 className="headline-lg text-slate-3">Add New Project</h1>
+        <Button className="w-fit flex items-center gap-2 rounded-xs!">
+          <AddMemberIcon />
+          <span>Invite Member</span>
+        </Button>
+      </div>
+      {project && <EditProjectForm project={project} />}
+    </div>
+  );
+}
+
+export default ProjectEditPage;
---
+++ src/components/common/projects/editProjectForm.tsx
@@ -0,0 +1,157 @@
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+
+import { toast } from "react-toastify";
+
+// icons
+import NewProjectIcon from "../../icons/newProjectIcon";
+
+// components
+import Input from "../../ui/input";
+import Button from "../../ui/button";
+
+// api
+import { editProjectApi } from "@/src/lib/api/projects/editProject";
+
+// validation libraries
+import { useForm, SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+// schemas
+import { addProjectSchema as editProjectSchema } from "@/src/lib/schemas/addProjectSchema";
+
+// types
+import { AddProjectFormData as EditProjectFromData } from "@/src/lib/schemas/addProjectSchema";
+import ProTipIcon from "../../icons/proTipIcon";
+import Textarea from "../../ui/textarea";
+import ValidationErrorIcon from "../../icons/validationErrorIcon";
+import Spinner from "../../ui/spinner";
+import { Project } from "@/src/types/projectType";
+
+interface ProjectEditProps {
+  project: Project;
+}
+
+const EditProjectForm = ({ project }: ProjectEditProps) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<EditProjectFromData>({
+    resolver: zodResolver(editProjectSchema),
+  });
+
+  const description = watch("description");
+
+  // form submition logic
+  const onSubmit: SubmitHandler<EditProjectFromData> = async (data) => {
+    setIsLoading(true);
+    try {
+      await editProjectApi(data, project.id);
+      toast.success("Project updated successfully!");
+    } catch {
+      toast.error(`Failed to update project: ${errors}`);
+    } finally {
+      setIsLoading(false); // resets the loading state after response
+    }
+  };
+
+  return (
+    <div className="sm:bg-white w-fit mx-auto rounded-lg my-10">
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-16 sm:p-8">
+        <div className="flex items-center gap-4">
+          <NewProjectIcon className="bg-primary-container/10 size-10 p-2 rounded-xs hidden sm:block" />
+          <div className="flex flex-col">
+            <h3 className="font-semibold text-2xl">Edit Project</h3>
+            <p className="text-slate-2">
+              Define the scope and foundational details of your project.
+            </p>
+          </div>
+        </div>
+        <div className="space-y-10">
+          {/* name field */}
+          <div className="flex flex-col gap-2">
+            <label htmlFor="name" className="uppercase label-sm text-slate-2">
+              project title <span className="text-error">*</span>
+            </label>
+            <Input
+              {...register("name")}
+              id="name"
+              type="text"
+              placeholder="Enter project title"
+              required
+              defaultValue={project.name}
+              variant={errors.name ? "error" : "primary"}
+            />
+            {errors.name && (
+              <div className="flex items-center gap-2">
+                <ValidationErrorIcon />
+                <span className="text-red-600 text-sm">
+                  {errors.name.message}
+                </span>
+              </div>
+            )}
+          </div>
+          {/* email field */}
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="email"
+              className="uppercase label-sm text-slate-2 flex justify-between items-center"
+            >
+              <span>description</span>
+              <span className="text-slate-1 capitalize">optional</span>
+            </label>
+            <Textarea
+              {...register("description")}
+              id="email"
+              defaultValue={project.description}
+              placeholder="Provide a high-level overview of the project's architectural objectives and key milestones..."
+              variant={errors.name ? "error" : "primary"}
+            />
+            <div className="flex flex-col sm:flex-row justify-between items-center">
+              {errors.description && (
+                <div className="flex items-center gap-2">
+                  <ValidationErrorIcon />
+                  <span className="text-red-600 text-sm">
+                    {errors.description.message}
+                  </span>
+                </div>
+              )}
+              <p className="text-slate-2 ml-auto">
+                {description?.length} / 500 characters
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col-reverse sm:flex-row gap-2 justify-between items-center">
+          <Link href={"/project"} className="w-full sm:w-fit">
+            <Button
+              variant="ghost"
+              className="hover:bg-slate-1 w-full sm:w-fit"
+            >
+              Back
+            </Button>
+          </Link>
+          <Button
+            disabled={isLoading}
+            className={`capitalize w-full sm:w-48 rounded-xs! flex items-center justify-center gap-2 text-center ${isLoading && `opacity-70 cursor-not-allowed!`}`}
+          >
+            {isLoading && <Spinner />}
+            <span>save changes</span>
+          </Button>
+        </div>
+      </form>
+      <p className="w-fit mx-auto flex flex-col sm:flex-row sm:items-center gap-2 bg-surface-low text-slate-2 label-sm p-4">
+        <ProTipIcon className="hidden sm:block" />
+        <span className="font-bold">Pro Tip:</span> You can invite project
+        members and assign epics immediately after the initial creation process.
+      </p>
+    </div>
+  );
+};
+
+export default EditProjectForm;
