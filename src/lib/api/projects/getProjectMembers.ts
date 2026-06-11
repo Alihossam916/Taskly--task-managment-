24d5fc3b45d946931bd6118ca44608d8dc23ff77
Alihossam916 <alihossam916@gmail.com>
2026-06-12 00:39:33 +0300

    feat: create get project members API function

---
+++ src/lib/api/projects/getProjectMembers.ts
@@ -0,0 +1,30 @@
+"use server";
+import { getAccessToken } from "../../utils/cookies";
+import { Project } from "@/src/types/projectType";
+
+const baseUrl = process.env.SUPABASE_URL!;
+const anonKey = process.env.SUPABASE_ANON_KEY!;
+
+export async function getProjectMembers(
+  projectId: string,
+): Promise<Project | null> {
+  const token = await getAccessToken();
+  try {
+    const response = await fetch(
+      `${baseUrl}/rest/v1/get_project_members?project_id=eq.${projectId}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          apikey: anonKey,
+          "Content-Type": "application/json",
+        },
+      },
+    );
+    if (!response.ok) return null;
+    const members = await response.json();
+    return members;
+  } catch {
+    return null;
+  }
+}
