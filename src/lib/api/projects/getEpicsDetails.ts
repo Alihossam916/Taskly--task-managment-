8ce87e3917ee592410931944ac5a689eca7b3671
Alihossam916 <alihossam916@gmail.com>
2026-06-16 20:22:17 +0300

    feat: create getEpicsDetails API function to fetch epic details

---
+++ src/lib/api/projects/getEpicsDetails.ts
@@ -0,0 +1,39 @@
+"use server";
+// utils
+import { getAccessToken } from "../../utils/cookies";
+
+// types
+import { Epic } from "@/src/types/projectType";
+
+const baseUrl = process.env.SUPABASE_URL!;
+const anonKey = process.env.SUPABASE_ANON_KEY!;
+
+export async function getEpicsDetails(
+  projectId: string,
+  epicId: string,
+): Promise<{ epic: Epic }> {
+  const token: string | null = await getAccessToken();
+
+  try {
+    const response = await fetch(
+      `${baseUrl}/rest/v1/project_epics?project_id=eq.${projectId}&id=eq.${epicId}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          apikey: anonKey,
+          "Content-Type": "application/json",
+        },
+      },
+    );
+    if (response.ok) {
+      const epic = await response.json();
+      return { epic };
+    }
+    throw new Error("Failed to fetch projects");
+  } catch (error) {
+    throw new Error(
+      error instanceof Error ? error.message : "Failed to fetch projects",
+    );
+  }
+}
