419ae0a4d534aaff0f6d814a893204d1a8e6997d
Alihossam916 <alihossam916@gmail.com>
2026-06-22 03:42:34 +0300

    feat: create API endpoint to fetch all tasks for a project

---
+++ src/lib/api/projects/getAllTasks.ts
@@ -0,0 +1,36 @@
+"use server";
+import { getAccessToken } from "../../utils/cookies";
+import { Task } from "@/src/types/projectType";
+
+const baseUrl = process.env.SUPABASE_URL!;
+const anonKey = process.env.SUPABASE_ANON_KEY!;
+
+export async function getAllTasksApi(
+  projectId: string,
+): Promise<Task[] | null> {
+  const token = await getAccessToken();
+  if (!token) {
+    throw new Error("unauthorized");
+  }
+
+  try {
+    const response = await fetch(
+      `${baseUrl}/rest/v1/project_tasks?project_id=eq.${projectId}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          apikey: anonKey,
+          "Content-Type": "application/json",
+        },
+      },
+    );
+    if (response.ok) {
+      const tasks: Task[] = await response.json();
+      return tasks;
+    }
+    return null;
+  } catch {
+    return null;
+  }
+}
