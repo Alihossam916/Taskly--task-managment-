1d88ca650f24812033130f3fe1b11d368999a1c4
Alihossam916 <alihossam916@gmail.com>
2026-06-22 21:30:59 +0300

    style: change style according to design for task mobile view

--- src/components/common/projects/tasksMobileView.tsx
+++ src/components/common/projects/tasksMobileView.tsx
@@ -8,11 +8,11 @@ import { Member, Task } from "@/src/types/projectType";
 import { getAllTasksApi } from "@/src/lib/api/projects/getAllTasks";
 import { getProjectMembers } from "@/src/lib/api/projects/getProjectMembers";
 
-// constants
-import { statuses } from "@/src/constants/taskStatuses";
+// utils
 import { getInitials } from "@/src/lib/utils/initials";
-import { metadata } from "@/src/app/layout";
 import { formatDate } from "@/src/lib/utils/formatDate";
+
+// icons
 import EditIcon from "../../icons/editIcon";
 
 const TasksMobileView = ({ projectId }: { projectId: string }) => {
@@ -40,19 +40,26 @@ const TasksMobileView = ({ projectId }: { projectId: string }) => {
         return (
           <div key={task.task_id} className="w-full bg-white p-4 space-y-4">
             <div className="flex items-center justify-between">
-              <h5>{task.task_id}</h5>
-              <p>{task.status}</p>
+              <h5 className="label-sm text-slate-2/70">{task.task_id}</h5>
+              <p
+                className={`label-sm p-2 rounded-xs ${task.status === "DONE" ? `bg-success` : task.status === `BLOCKED` ? `bg-error/20 text-error` : `bg-surface-highest`}`}
+              >
+                {task.status.replace("_", " ")}
+              </p>
             </div>
-            <h3>{task.title}</h3>
+            <h3 className="title-md text-slate-3 capitalize">{task.title}</h3>
             <div className="flex items-center justify-between">
               <div className="flex items-center gap-2">
-                <span className="bg-primary-container text-white size-9 p-2 flex justify-center items-center rounded-full"> {getInitials(assignee?.metadata.name) || "UN"}</span>
+                <span className="bg-primary-container text-white size-9 p-2 flex justify-center items-center rounded-full">
+                  {" "}
+                  {getInitials(assignee?.metadata.name) || "UN"}
+                </span>
                 <div>
-                  <p className="uppercase">due date</p>
+                  <p className="uppercase label-sm text-slate-2/70">due date</p>
                   <p>{formatDate(task.due_date)}</p>
                 </div>
               </div>
-              <EditIcon />
+              <EditIcon className="text-slate-2/70 p-2 size-8" />
             </div>
           </div>
         );
