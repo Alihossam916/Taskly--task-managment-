03e44470e10425bdec59a643aa8d718d4a5d9a19
Alihossam916 <alihossam916@gmail.com>
2026-06-25 23:13:58 +0300

    refactor: separate task card into its own component for mobile view

--- src/components/common/projects/tasksMobileView.tsx
+++ src/components/common/projects/tasksMobileView.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 
 // components
@@ -15,19 +15,9 @@ import { Member, Task, TaskViewProps } from "@/src/types/projectType";
 import { getAllTasksApi } from "@/src/lib/api/projects/getAllTasks";
 import { getProjectMembers } from "@/src/lib/api/projects/getProjectMembers";
 
-// utils
-import { getInitials } from "@/src/lib/utils/initials";
-import { formatDate } from "@/src/lib/utils/formatDate";
-
-// icons
-import EditIcon from "../../icons/editIcon";
-
-// redux
-import { useDispatch } from "react-redux";
-import { openTaskDetails } from "@/src/lib/redux/feature/taskModalSlice";
-
 // hooks
 import { useInfiniteScroll } from "@/src/hooks/useInfiniteScroll";
+import TaskMobileCard from "../../ui/taskMobileCard";
 
 const TasksMobileView = ({
   projectId,
@@ -37,7 +27,6 @@ const TasksMobileView = ({
 }: TaskViewProps) => {
   const [tasks, setTasks] = useState<Task[] | null>([]);
   const [members, setMembers] = useState<Member[]>([]);
-  const dispatch = useDispatch();
   const [totalTasks, setTotalTasks] = useState<number>(0);
   const [initialLoading, setInitialLoading] = useState(true);
   const [hasError, setHasError] = useState(false);
@@ -148,42 +137,13 @@ const TasksMobileView = ({
         const assignee = members.find((m) => m.user_id === task.assignee?.id);
 
         return (
-          <div
-            key={task.task_id}
-            onClick={() =>
-              dispatch(
-                openTaskDetails({
-                  taskId: task.id,
-                  projectId,
-                  epicId: task.epic_id,
-                }),
-              )
-            }
-            className="w-full bg-white p-4 space-y-4"
-          >
-            <div className="flex items-center justify-between">
-              <h5 className="label-sm text-slate-2/70">{task.task_id}</h5>
-              <p
-                className={`label-sm p-2 rounded-xs ${task.status === "DONE" ? `bg-success` : task.status === `BLOCKED` ? `bg-error/20 text-error` : `bg-surface-highest`}`}
-              >
-                {task.status.replace("_", " ")}
-              </p>
-            </div>
-            <h3 className="title-md text-slate-3 capitalize">{task.title}</h3>
-            <div className="flex items-center justify-between">
-              <div className="flex items-center gap-2">
-                <span className="bg-primary-container text-white size-9 p-2 flex justify-center items-center rounded-full">
-                  {" "}
-                  {getInitials(assignee?.metadata.name) || "UN"}
-                </span>
-                <div>
-                  <p className="uppercase label-sm text-slate-2/70">due date</p>
-                  <p>{formatDate(task.due_date)}</p>
-                </div>
-              </div>
-              <EditIcon className="text-slate-2/70 p-2 size-8" />
-            </div>
-          </div>
+          <Fragment key={task.task_id}>
+            <TaskMobileCard
+              task={task}
+              assignee={assignee}
+              projectId={projectId}
+            />
+          </Fragment>
         );
       })}
       <InfiniteScrollLoader
---
+++ src/components/ui/taskMobileCard.tsx
@@ -0,0 +1,61 @@
+// utils
+import { getInitials } from "@/src/lib/utils/initials";
+import { formatDate } from "@/src/lib/utils/formatDate";
+
+// icons
+import EditIcon from "../icons/editIcon";
+
+// redux
+import { useDispatch } from "react-redux";
+import { openTaskDetails } from "@/src/lib/redux/feature/taskModalSlice";
+import { Member, Task } from "@/src/types/projectType";
+
+interface TaskMobileCardProp {
+  task: Task;
+  assignee?: Member;
+  projectId: string;
+}
+
+const TaskMobileCard = ({ task, assignee, projectId }: TaskMobileCardProp) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div
+      onClick={() =>
+        dispatch(
+          openTaskDetails({
+            taskId: task.id,
+            projectId,
+            epicId: task.epic_id,
+          }),
+        )
+      }
+      className="w-full bg-white p-4 space-y-4"
+    >
+      <div className="flex items-center justify-between">
+        <h5 className="label-sm text-slate-2/70">{task.task_id}</h5>
+        <p
+          className={`label-sm p-2 rounded-xs ${task.status === "DONE" ? `bg-success` : task.status === `BLOCKED` ? `bg-error/20 text-error` : `bg-surface-highest`}`}
+        >
+          {task.status.replace("_", " ")}
+        </p>
+      </div>
+      <h3 className="title-md text-slate-3 capitalize">{task.title}</h3>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <span className="bg-primary-container text-white size-9 p-2 flex justify-center items-center rounded-full">
+            {" "}
+            {getInitials(assignee?.metadata.name) || "UN"}
+          </span>
+          <div>
+            <p className="uppercase label-sm text-slate-2/70">due date</p>
+            <p>{formatDate(task.due_date)}</p>
+          </div>
+        </div>
+        <EditIcon className="text-slate-2/70 p-2 size-8" />
+      </div>
+    </div>
+  );
+};
+
+export default TaskMobileCard;
